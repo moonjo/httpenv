a9aa0a092341069c8b1f9bafa85455277cdf883d
Sean Kim <moonbal@gmail.com>
2025-04-11 13:42:08 -0400

    test for cve2

--- httpenv.go
+++ httpenv.go
@@ -7,7 +7,6 @@ import (
   "strings"
 )
 func serve(w http.ResponseWriter, r *http.Request) {
-  var password = "mysecretpw"
   env := map[string]string{}
   for _, keyval := range os.Environ() {
     keyval := strings.SplitN(keyval, "=", 2)
@@ -21,6 +20,7 @@ func serve(w http.ResponseWriter, r *http.Request) {
   w.Write([]byte(bytes))
 }
 func main() {
+  var password string = "mysecretpw"
   fmt.Printf("Starting httpenv listening on port 8888.\n")
   http.HandleFunc("/", serve)
   if err := http.ListenAndServe(":8888", nil); err != nil {
