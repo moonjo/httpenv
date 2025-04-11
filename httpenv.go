a6109daca8f5499d7caddd65ba00e767e257e733
Sean Kim <moonbal@gmail.com>
2025-04-11 13:48:57 -0400

    test for cve4

--- httpenv.go
+++ httpenv.go
@@ -20,7 +20,6 @@ func serve(w http.ResponseWriter, r *http.Request) {
   w.Write([]byte(bytes))
 }
 func main() {
-  var fubar string = "aybabtu"
   fmt.Printf("Starting httpenv listening on port 8888.\n")
   http.HandleFunc("/", serve)
   if err := http.ListenAndServe(":8888", nil); err != nil {
